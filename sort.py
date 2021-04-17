6d72f3cf70a68eb23ad8fdc0d39e4e1b7af4746f
wadayamada <wada0421514@gmail.com>
2021-04-17 10:43:33 +0900

    クイックソート

--- sort.py
+++ sort.py
@@ -17,6 +17,21 @@ def selection_sort(arr):
         min_index=i+search_min_index(arr[i:])
         swap(i,min_index,arr)
 
+def quick_sort(arr):
+    if len(arr)<=1:
+        return arr
+    pivot_index=len(arr)//2
+    left=[]
+    right=[]
+    for i in range(len(arr)):
+        if arr[i] <= arr[pivot_index] and i!=pivot_index:
+            left.append(arr[i])
+        if arr[i] > arr[pivot_index]:
+            right.append(arr[i])
+    left=quick_sort(left)
+    right=quick_sort(right)
+    return left+[arr[pivot_index]]+right
+
 if __name__=='__main__':
 
     arr=[3,5,2,4,1]
@@ -24,3 +39,7 @@ if __name__=='__main__':
     arr_for_ss=copy.deepcopy(arr)
     selection_sort(arr_for_ss)
     print(f'selection_sort: {arr_for_ss}')
+
+    arr_for_qs=copy.deepcopy(arr)
+    arr_for_qs=quick_sort(arr_for_qs)
+    print(f'quick_sort    : {arr_for_qs}')
