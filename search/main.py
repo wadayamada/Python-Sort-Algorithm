d073afcafcee13a1961006a9f9a065350f458e74
wadayamada <wada0421514@gmail.com>
2021-04-21 22:24:47 +0900

    print文、見やすくした

--- search/main.py
+++ search/main.py
@@ -6,7 +6,7 @@ if __name__=='__main__':
     arr=[3,5,2,4,1,6]
     target=4
 
-    print(linear_sort(arr,target))
+    print(f'linea_search  : index={linear_sort(arr,target)}')
 
     sorted_arr=sorted(arr)
-    print(binary_search(sorted_arr,target))
+    print(f'binary_search : index={binary_search(sorted_arr,target)}')
